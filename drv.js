aca67624b340f79a03318c5c7ab33f344e81f43a
Drv7.ID <137008530+Drvee07@users.noreply.github.com>
2023-10-20 07:06:56 +0700

    drv.js

--- drv.js
+++ drv.js
@@ -1,7 +1,7 @@
 // ****************************************************************
 // ************************** BACKGROUND ***************************
 // ****************************************************************
-var c = document.getElementById("canvas");
+var c = document.getElementById("bg");
 var ctx = c.getContext("2d");
 
 //making the canvas full screen
